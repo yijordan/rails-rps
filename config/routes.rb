593d36c4969a50e472ccae7f3d43de9be5111fbd
yijordan <yij@uchicago.edu>
2025-02-25 03:21:29 +0000

    updated commentary

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  # get("/", {})
+  
   get("/rock", { :controller => "rochambeau", :action => "rock"})
 
   get("/paper", { :controller => "rochambeau", :action => "paper"})
 
-  get("/scissors", { :controller => "rochambeau", :action =>} "scissors"})
-  
+  get("/scissors", { :controller => "rochambeau", :action => "scissors"})
+
 end
