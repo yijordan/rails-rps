70394fd5a39389ce114bf430f79c29f1ebf1e191
yijordan <yij@uchicago.edu>
2025-02-25 03:22:55 +0000

    updated commentary for win status (for scissors only)

--- app/controllers/rochambeau_controller.rb
+++ app/controllers/rochambeau_controller.rb
@@ -11,6 +11,13 @@ class RochambeauController < ApplicationController
     
   def scissors
     @com_move = ["rock", "paper", "scissors"].sample
+    if @com_move == "rock"
+      @outcome = "lost"
+    elsif @com_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
     render({ :template => "rochambeau_templates/scissors"})
   end
 end
